929f287156ff44a7ab066d5ec6586b51ac4ddf7f
AhmmedAshek <ashek.ahmmed@sydney.edu.au>
2024-03-07 20:51:37 +1100

    contains flipping function for name :)

--- hello.c
+++ hello.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+
+char* flipAString(char*, int);
 
 int main()
 {
@@ -7,5 +10,21 @@ int main()
 	scanf("%s", &name);
 	printf("Welcome %s!\n", name);
 	printf("Your name contains %d letters.\n", strlen(name));
+	printf("Flipping your name produces: %s.\n",flipAString(name, strlen(name)));
 	return 0;
+}
+
+char* flipAString(char* name, int length)
+{
+	int i;
+	for(i = 0; i<(length/2); i++)
+		{
+			//printf("%c\n", *(name+i));
+			//swap(name[i],name[length-1-i]);
+			char tmp = name[length-1-i];
+			name[length-1-i] = name[i];
+			name[i] = tmp;
+		}
+			
+	return name;
 }
